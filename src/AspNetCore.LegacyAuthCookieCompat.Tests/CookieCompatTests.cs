386a41400fcba9c52020e97837c6e811ebb1faa1
Tom Hofman <tom.hofman@gmail.com>
2017-02-27 23:25:42 +0100

    Removed nunit test dependency in favor of 'default' using Microsoft.VisualStudio.TestTools.UnitTesting

--- src/AspNetCore.LegacyAuthCookieCompat.Tests/CookieCompatTests.cs
+++ src/AspNetCore.LegacyAuthCookieCompat.Tests/CookieCompatTests.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Security;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AspNetCore.LegacyAuthCookieCompat.Tests
 {
-
-
-    [TestFixture]
+	[TestClass]
     public class CookieCompatTests
     {
         // These would come from the asp.net 3.5 applications web.config.
@@ -19,38 +12,38 @@ namespace AspNetCore.LegacyAuthCookieCompat.Tests
         private string _DecryptionKeyText = "AC7387D7E54B156377D81930CF237888854B5B5B515CF2D6356541255E696144";
 
        
-        [Test]
+        [TestMethod]
         public void Can_Encrypt_And_Decrypt_Forms_Authentication_Ticket()
         {
            
             // Arrange
             var issueDate = new DateTime(2015, 12, 22, 15, 09, 25);
-            var expiryDate = new DateTime(0001, 01, 01, 00, 00, 00);
-            var authTicket = new FormsAuthenticationTicket(2, "someuser@some-email.com", issueDate, expiryDate, false, "custom data", "/");
+            var expiryDate = new DateTime(2018, 01, 01, 00, 00, 00);
+            var formsAuthenticationTicket = new FormsAuthenticationTicket(2, "someuser@some-email.com", issueDate, expiryDate, false, "custom data", "/");
+			
+            var legacyFormsAuthenticationTicketEncryptor = new LegacyFormsAuthenticationTicketEncryptor(_DecryptionKeyText, _ValidationKeyText);
 
-            var sha1Hasher = new Sha1HashProvider(_ValidationKeyText);
-            var sut = new LegacyFormsAuthenticationTicketEncryptor(_DecryptionKeyText);
-
-            // Act
-            // We encrypt the forms auth cookie.
-            var encryptedText = sut.Encrypt(authTicket, sha1Hasher);
+			// Act
+			// We encrypt the forms auth cookie.
+			byte[] validationKeyBytes = HexUtils.HexToBinary(_ValidationKeyText);
+			var sha1Hasher = new Sha1HashProvider(validationKeyBytes);
+			var encryptedText = legacyFormsAuthenticationTicketEncryptor.Encrypt(formsAuthenticationTicket, sha1Hasher);
             
-            Assert.NotNull(encryptedText);
+            Assert.IsNotNull(encryptedText);
             Console.Write(encryptedText);
 
             // We decrypt the encypted text back into a forms auth ticket, and compare it to the original ticket to make sure it
             // roundtripped successfully.
-            FormsAuthenticationTicket decryptedTicket = sut.DecryptCookie(encryptedText, new Sha1HashProvider(_ValidationKeyText));
+            FormsAuthenticationTicket decryptedFormsAuthenticationTicket = legacyFormsAuthenticationTicketEncryptor.DecryptCookie(encryptedText);
 
-            Assert.That(() => authTicket.CookiePath, Is.EqualTo(decryptedTicket.CookiePath));
-            Assert.That(() => authTicket.Expiration, Is.EqualTo(decryptedTicket.Expiration));
-            Assert.That(() => authTicket.Expired, Is.EqualTo(decryptedTicket.Expired));
-            Assert.That(() => authTicket.IsPersistent, Is.EqualTo(decryptedTicket.IsPersistent));
-            Assert.That(() => authTicket.IssueDate, Is.EqualTo(decryptedTicket.IssueDate));
-            Assert.That(() => authTicket.UserData, Is.EqualTo(decryptedTicket.UserData));
-            Assert.That(() => authTicket.Version, Is.EqualTo(decryptedTicket.Version));
-
-        }
+			Assert.AreEqual(formsAuthenticationTicket.CookiePath, decryptedFormsAuthenticationTicket.CookiePath);
+			Assert.AreEqual(formsAuthenticationTicket.Expiration, decryptedFormsAuthenticationTicket.Expiration);
+			Assert.AreEqual(formsAuthenticationTicket.Expired, decryptedFormsAuthenticationTicket.Expired);
+			Assert.AreEqual(formsAuthenticationTicket.IsPersistent, decryptedFormsAuthenticationTicket.IsPersistent);
+			Assert.AreEqual(formsAuthenticationTicket.IssueDate, decryptedFormsAuthenticationTicket.IssueDate);
+			Assert.AreEqual(formsAuthenticationTicket.UserData, decryptedFormsAuthenticationTicket.UserData);
+			Assert.AreEqual(formsAuthenticationTicket.Version, decryptedFormsAuthenticationTicket.Version);
+		}
     }
 
 }
