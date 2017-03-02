bbd8860d7ff55e1318028f6941a1bbe63c7c2414
dazinator <darrell.tunnell@googlemail.com>
2017-03-02 23:08:05 +0000

    Closes #4 - netstandard 1.3

---
+++ src/AspNetCore.LegacyAuthCookieCompat/HexUtils.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AspNetCore.LegacyAuthCookieCompat
+{
+	public static class HexUtils
+	{
+		/// <summary>
+		/// Converts a byte array into it's string representation.
+		/// </summary>
+		/// <param name="data">byte array</param>
+		/// <returns>The byte array corresponding to the contents of the hex string,
+		/// or null if the input string is not a valid hex string.</returns>
+		public static string BinaryToHex(byte[] data)
+		{
+			if (data == null)
+			{
+				return null;
+			}
+
+			char[] hex = new char[checked(data.Length * 2)];
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				byte thisByte = data[i];
+				hex[2 * i] = NibbleToHex((byte)(thisByte >> 4)); // high nibble
+				hex[2 * i + 1] = NibbleToHex((byte)(thisByte & 0xf)); // low nibble
+			}
+
+			return new string(hex);
+		}
+
+		/// <summary>
+		/// Converts a hexadecimal string into it's binary representation.
+		/// </summary>
+		/// <param name="data">The hex string.</param>
+		/// <returns>The byte array corresponding to the contents of the hex string,
+		/// or null if the input string is not a valid hex string.</returns>
+		public static byte[] HexToBinary(string data)
+		{
+			if (data == null || data.Length % 2 != 0)
+			{
+				// input string length is not evenly divisible by 2
+				return null;
+			}
+
+			byte[] binary = new byte[data.Length / 2];
+
+			for (int i = 0; i < binary.Length; i++)
+			{
+				int highNibble = HexToInt(data[2 * i]);
+				int lowNibble = HexToInt(data[2 * i + 1]);
+
+				if (highNibble == -1 || lowNibble == -1)
+				{
+					return null; // bad hex data
+				}
+				binary[i] = (byte)((highNibble << 4) | lowNibble);
+			}
+
+			return binary;
+		}
+
+		public static int HexToInt(char h)
+		{
+			return (h >= '0' && h <= '9') ? h - '0' :
+			(h >= 'a' && h <= 'f') ? h - 'a' + 10 :
+			(h >= 'A' && h <= 'F') ? h - 'A' + 10 :
+			-1;
+		}
+
+		// converts a nibble (4 bits) to its uppercase hexadecimal character representation [0-9, A-F]
+		private static char NibbleToHex(byte nibble)
+		{
+			return (char)((nibble < 10) ? (nibble + '0') : (nibble - 10 + 'A'));
+		}
+	}
+
+}
---
+++ src/AspNetCore.LegacyAuthCookieCompat/Sha1HashProvider.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace AspNetCore.LegacyAuthCookieCompat
+{
+	public class Sha1HashProvider
+    {
+        public const int SHA1_HASH_SIZE = 20;
+        public const int SHA1_KEY_SIZE = 64;
+
+        private static int _HashSize;
+        private static int _KeySize;
+
+        private byte[] _validationKeyBlob;
+
+        private byte[] _inner = null;
+        private byte[] _outer = null;
+
+        public Sha1HashProvider(byte[] validationKey, int hashSize = SHA1_HASH_SIZE, int keySize = SHA1_KEY_SIZE)
+        {
+            _HashSize = hashSize;
+            _KeySize = keySize;
+            _validationKeyBlob = validationKey;
+            SetInnerOuterKeys(_validationKeyBlob, ref _inner, ref _outer);
+        }
+
+
+        public byte[] GetHMACSHA1Hash(byte[] buf, byte[] modifier, int start, int length)
+        {
+            if (start < 0 || start > buf.Length)
+                throw new ArgumentException("start");
+            if (length < 0 || buf == null || (start + length) > buf.Length)
+                throw new ArgumentException("length");
+
+			var hmacsha1Hasher = new HMACSHA1(_validationKeyBlob);
+
+			byte[] hash = hmacsha1Hasher.ComputeHash(buf, start, length);
+
+			return hash;
+		}
+
+        public byte[] CheckHashAndRemove(byte[] bufHashed)
+        {
+            if (!CheckHash(bufHashed, bufHashed.Length - _HashSize))
+                return null;
+
+            byte[] buf2 = new byte[bufHashed.Length - _HashSize];
+            Buffer.BlockCopy(bufHashed, 0, buf2, 0, buf2.Length);
+            return buf2;
+        }
+
+        public bool CheckHash(byte[] decryptedCookie, int hashIndex)
+        {
+            // 2. SHA1 Hash is appended to the end.
+            // Verify the hash matches by re-computing the hash for this message, and comparing.
+            byte[] hashCheckBlob = GetHMACSHA1Hash(decryptedCookie, null, 0, hashIndex);
+            if (hashCheckBlob == null)
+            {
+                throw new Exception();
+            }
+
+            //////////////////////////////////////////////////////////////////////
+            // Step 2: Make sure the MAC has expected length
+            if (hashCheckBlob == null || hashCheckBlob.Length != _HashSize)
+                throw new Exception();
+
+
+            // To prevent a timing attack, we should verify the entire hash instead of failing
+            // early the first time we see a mismatched byte.            
+            bool hashCheckFailed = false;
+            for (int i = 0; i < _HashSize; i++)
+            {
+                if (hashCheckBlob[i] != decryptedCookie[hashIndex + i])
+                {
+                    hashCheckFailed = true;
+                }
+            }
+
+            return !hashCheckFailed;
+        }
+
+        private static void SetInnerOuterKeys(byte[] validationKey, ref byte[] inner, ref byte[] outer)
+        {
+            byte[] key = null;
+            if (validationKey.Length > _KeySize)
+            {
+                key = new byte[_HashSize];
+
+				var hmacsha1Hasher = new HMACSHA1(validationKey);
+				hmacsha1Hasher.ComputeHash(key);
+			}
+
+            if (inner == null)
+                inner = new byte[_KeySize];
+            if (outer == null)
+                outer = new byte[_KeySize];
+
+            int i;
+            for (i = 0; i < _KeySize; i++)
+            {
+                inner[i] = 0x36;
+                outer[i] = 0x5C;
+            }
+            for (i = 0; i < validationKey.Length; i++)
+            {
+                inner[i] ^= validationKey[i];
+                outer[i] ^= validationKey[i];
+            }
+        }
+
+        public int HashSize { get { return _HashSize; } }
+
+        public byte[] GetIVHash(byte[] buf, int ivLength)
+        {
+            // return an IV that is computed as a hash of the buffer
+            int bytesToWrite = ivLength;
+            int bytesWritten = 0;
+            byte[] iv = new byte[ivLength];
+
+            // get SHA1 hash of the buffer and copy to the IV.
+            // if hash length is less than IV length, re-hash the hash and
+            // append until IV is full.
+            byte[] hash = buf;
+            while (bytesWritten < ivLength)
+            {
+                byte[] newHash = new byte[_HashSize];
+
+				var hmacsha1Hasher = new HMACSHA1(_validationKeyBlob);
+				newHash = hmacsha1Hasher.ComputeHash(hash);
+
+				int bytesToCopy = Math.Min(_HashSize, bytesToWrite);
+                Buffer.BlockCopy(hash, 0, iv, bytesWritten, bytesToCopy);
+
+                bytesWritten += bytesToCopy;
+                bytesToWrite -= bytesToCopy;
+            }
+            return iv;
+        }
+
+
+    }
+}
