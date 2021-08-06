80783dfe073ee39b298439f8cab90917c4736be7
Stephan Noack <stephan.noack@cortado.com>
2021-08-06 13:07:29 +0200

    Sha384 support

---
+++ src/AspNetCore.LegacyAuthCookieCompat/Sha384HashProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace AspNetCore.LegacyAuthCookieCompat
+{
+    internal class Sha384HashProvider : HashProvider
+    {
+        private const int Sha1HashSize = 48;
+        private const int Sha1KeySize = 384;
+
+        public Sha384HashProvider(byte[] validationKey)
+            : base(validationKey, Sha1HashSize, Sha1KeySize)
+        {
+        }
+
+        protected override HMAC CreateHasher(byte[] key) => new HMACSHA384(key);
+    }
+}
