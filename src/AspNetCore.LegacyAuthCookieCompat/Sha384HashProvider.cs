25e3afb3c55b546f14bf39c39f48d5f74d971b3d
Stephan Noack <stephan.noack@cortado.com>
2021-08-09 10:12:35 +0200

    fixed Sha384 constants names

--- src/AspNetCore.LegacyAuthCookieCompat/Sha384HashProvider.cs
+++ src/AspNetCore.LegacyAuthCookieCompat/Sha384HashProvider.cs
@@ -4,11 +4,11 @@ namespace AspNetCore.LegacyAuthCookieCompat
 {
     internal class Sha384HashProvider : HashProvider
     {
-        private const int Sha1HashSize = 48;
-        private const int Sha1KeySize = 384;
+        private const int Sha384HashSize = 48;
+        private const int Sha384KeySize = 384;
 
         public Sha384HashProvider(byte[] validationKey)
-            : base(validationKey, Sha1HashSize, Sha1KeySize)
+            : base(validationKey, Sha384HashSize, Sha384KeySize)
         {
         }
 
